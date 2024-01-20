0dceac31e29afc69131330f14d5e9b30efaf91d4
Mesoma <lumiereee.du.soleil@gmail.com>
2024-01-19 21:41:55 -0500

    Fixed the name error!

--- LSP_Intro_Assignment/src/org/howard/edu/lsp/hw1/HelloWorld.java
+++ LSP_Intro_Assignment/src/org/howard/edu/lsp/hw1/HelloWorld.java
@@ -1,8 +1,12 @@
+/**
+ * Nwankwo Mesomachukwu 
+ */
 package org.howard.edu.lsp.hw1;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello World");
 	}
 
