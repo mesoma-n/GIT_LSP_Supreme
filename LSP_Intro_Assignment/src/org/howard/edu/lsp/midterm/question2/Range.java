e8b5f7546020fad6a1b58a15ac1810392d38dc93
Mesoma <lumiereee.du.soleil@gmail.com>
2024-03-14 20:32:50 -0400

    fixed part 2

--- LSP_Intro_Assignment/src/org/howard/edu/lsp/midterm/question2/Range.java
+++ LSP_Intro_Assignment/src/org/howard/edu/lsp/midterm/question2/Range.java
@@ -1,26 +1,63 @@
 package org.howard.edu.lsp.midterm.question2;
 
+/**
+ * An interface representing a range of integers.
+ *
+ */
 public interface Range {
-	// Returns true if v is ≥ lower bound and ≤ upper bound and false otherwise
-	public boolean contains( int value ); 
-	// Returns true if the receiver contains at least 
-	// one value in common with other, and false otherwise
-	public boolean overlaps( Range other );
-	// // Returns the number of integers in the range
-	public int size(); 
-	}
 
-class IntegerRange implements Range{
+    /**
+     * Checks if a given value is within the range.
+     * @param value The value to check.
+     * @return True if the value is within the range, false otherwise.
+     */
+    public boolean contains(int value);
 
-	    private final int stop;
-	    private final int start;
+    /**
+     * Checks if this range overlaps with another range.
+     * @param other The other range to check for overlap.
+     * @return if the receiver contains at least one value in common with other, and false otherwise public boolean overlaps( Range other ).
+     * @throws IllegalArgumentException if the other range is not of the same type.
+     */
+    
+    public boolean overlaps(Range other);
 
-	    public IntegerRange(int stop, int start) {
-	        if (stop > start) {
-	            throw new IllegalArgumentException("The math isn't right");
-	        }
-	        this.stop = stop;
-	        this.start = start;
+    /**
+     * Returns the number of integers in the range.
+     * @return The size of the range.
+     */
+    public int size();
+}
+
+/**
+ * An implementation of the Range interface
+ *
+ */
+class IntegerRange implements Range {
+
+    /**
+     * The lower bound of the range (inclusive).
+     */
+    private final int stop;
+
+    /**
+     * The upper bound of the range (inclusive).
+     */
+    private final int start;
+
+    /**
+     * Creates a new integer range with the specified start and stop values.
+     *
+     * @param stop The lower bound of the range.
+     * @param start The upper bound of the range.
+     * @throws IllegalArgumentException if the start value is greater than the stop value.
+     */
+    public IntegerRange(int stop, int start) {
+        if (stop > start) {
+            throw new IllegalArgumentException("The start value must be less than or equal to the stop value");
+        }
+        this.stop = stop;
+        this.start = start;
 	    }
 
 	    @Override
@@ -34,7 +71,7 @@ class IntegerRange implements Range{
 	            IntegerRange otherRange = (IntegerRange) other;
 	            return this.contains(otherRange.stop) || otherRange.contains(this.stop);
 	        } else {
-	            throw new IllegalArgumentException("BooHoo you");
+	            throw new IllegalArgumentException("BooHoo you!");
 	        }
 	    }
 
